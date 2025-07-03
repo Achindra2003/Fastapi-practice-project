d7830816685a408b2f0420ba01fd63475a118d37
achindra-sharma <achindrasharma.outlook.com>
2025-07-03 11:49:16 +0530

    Initial commit

---
+++ frontend/src/auth/ClerkProviderWithRoutes.jsx
@@ -0,0 +1,17 @@
+import {ClerkProvider} from "@clerk/clerk-react"
+import {BrowserRouter} from "react-router-dom";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+export default function ClerkProviderWithRoutes({children}){
+    console.log("THE KEY BEING PASSED TO CLERK IS:", PUBLISHABLE_KEY);
+    return (
+        <ClerkProvider publishableKey="{PUBLISHABLE_KEY}">
+            <BrowserRouter>{children}</BrowserRouter>
+        </ClerkProvider>
+    );
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ClerkProviderWithRoutes from './auth/ClerkProviderWithRoutes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ClerkProviderWithRoutes>
+      <App />
+    </ClerkProviderWithRoutes>
+  </React.StrictMode>,
+)
